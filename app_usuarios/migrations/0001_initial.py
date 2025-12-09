1b02dbb6761f0485092ab946a8fa50354cf0c47c
Juanes2006 <juanestebanmarulanda117@gmail.com>
2025-12-09 15:25:36 -0500

    CAMBIOS SUBIDOS 10

--- app_usuarios/migrations/0001_initial.py
+++ app_usuarios/migrations/0001_initial.py
@@ -16,6 +16,7 @@ class Migration(migrations.Migration):
             name='Usuario',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('telefono', models.CharField(blank=True, max_length=20, null=True)),
                 ('username', models.CharField(max_length=150, unique=True)),
                 ('first_name', models.CharField(max_length=150)),
                 ('last_name', models.CharField(max_length=150)),
